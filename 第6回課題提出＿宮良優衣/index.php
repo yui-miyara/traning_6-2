020e0784d1e0361ddde91bba1869c223478fdc1d
yui <yui7162bayuus2013@gmail.com>
2024-03-22 12:56:51 +0900

    content1完成

---
+++ 第6回課題提出＿宮良優衣/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="./css/style.css">
+  <link href="http://fonts.googleapis.com/earlyaccess/notosansjp.css">
+  <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+</head>
+<body>
+  <div class="main_content">
+    <div class="content_1" id="img">
+      <div class="logo"></div>
+      <p class="text1">日本最大規模のフリーランスイベント</p>
+      <img src="./images/kv2.png" width="50%" height="auto"></img>
+      <img src="./images/dym_logo.svg" width="50%" height="auto" class="dym_logo"></img>
+      <img src="./images/sub_title.png" width="50%" height="auto" class="sub_title"></img>
+      <div class="date">3/4<span>sat</span>10:00-18:00</div>
+      <p class="text2">東京国際フォーラム ホールＥ２</p>
+      <div class="linebutton"></div>
+    </div>
+  </div>
+</body>
+</html>
