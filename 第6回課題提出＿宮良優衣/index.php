fa0e12c42b47854487ad0533b9ca30a7048981ef
yui <yui7162bayuus2013@gmail.com>
2024-03-24 23:27:29 +0900

    content2完成

--- 第6回課題提出＿宮良優衣/index.php
+++ 第6回課題提出＿宮良優衣/index.php
@@ -21,6 +21,56 @@
       <p class="text2">東京国際フォーラム ホールＥ２</p>
       <div class="linebutton"></div>
     </div>
+    <div class="content_2">
+      <div class="summit">
+        <p>Freelance Summit</p>
+      </div>
+      <div class="undertext"></div>
+      <div class="btn">
+        <p class="text3">「FREELANCE SUMMIT」は、StockSun株式会社が主催する日本最大規模のフリーランスイベントです。</p>
+        <button class="sidebutton"></button>
+      </div>
+      <div class="text4">
+        <p>各業界でTOPクラスのフリーランスが集結するイベント。有名企業が集結する40ブースもの出展エリアに加え、各領域で圧倒的に結果を出し続ける精鋭フリーランス達が、自身のノウハウを語ります。他のイベントでは味わえない、新たな体験と出会える充実したコンテンツを用意しています。</p>
+      </div>
+      <div class="evn">
+        <p>Event Summary</p>
+      </div>
+      <div class="undertext2"></div>
+      <div class="content">
+        <div class="date2">
+          <div class="content_img">開催日程</div>
+          <p>2023月4日(土)開場10:00閉場18:00(受付開始9:30-)</p>
+        </div>
+        <div class="place">
+        <div class="content_img">会場</div>
+          <p>東京国際フォーラム ホールE2</p>
+        </div>
+        <div class="event">
+        <div class="content_img">イベント内容</div>
+          <p>企業ブース:60ブース<br>ビジネス系セミナー:12枠</p>
+        </div>
+        <div class="target">
+        <div class="content_img">対象者</div>
+          <p>経営者/フリーランス/独立志望の会社員</p>
+        </div>
+        <div class="access">
+        <div class="content_img">アクセス</div>
+          <div>
+            <p class="text5">[最寄り駅]</p>
+            <p class="text6"><img src="./images/Ellipse 33.png" width="10" height="10" class="img2"></img>JR<br>有楽町駅より徒歩１分<br>東京駅より徒歩５分<br>
+            <img src="./images/Ellipse 33.png" width="10" height="10" class="img2"></img>地下鉄<br>有楽町駅 : 有楽町駅(B1地下コンコースにて連絡)
+            <br>千代田線 : 二重前橋駅より徒歩５分/日比谷駅より徒歩７分
+            <br>丸の内線 : 銀座駅より徒歩５分
+            <br>銀座線 : 銀座駅より徒歩７分/京橋駅より徒歩７分
+            <br>三田線 : 日比谷駅より徒歩５分
+            </p>
+          </div>
+          <img class="img3"></img>
+        </div>
+        <img class="img3_1"></img>
+      </div>
+    </div>
   </div>
 </body>
 </html>
